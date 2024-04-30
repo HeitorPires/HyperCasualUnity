6b798e4aca5fc949984ed3423be2921233caf06d
HeitorPires <heitorpprocopio@hotmail.com>
2024-04-30 15:47:14 -0400

    Add Enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 1f;
+
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
+    public string tagToCheckEnemy = "Enemy";
+
+    private bool _canRun;
+    private Vector3 _pos;
+
+
+    private void Start()
+    {
+        _canRun = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+
+        if(!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        transform.Translate(speed * Time.deltaTime * transform.forward);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
+    }
+
 }
